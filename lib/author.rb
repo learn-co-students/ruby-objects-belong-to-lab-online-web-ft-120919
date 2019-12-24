ddcdfe8f10c86061e704b6c03e4f05ba20bde323
Ayako Wiltshire <awittyl44@gmail.com>
2019-12-24 23:50:35 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,11 @@
+
+
+class Author
+  
+  attr_accessor :name
+  
+  def initialize(name= "Uncle Bob")
+    @name = name
+  end
+  
+end
---
+++ lib/song.rb
@@ -0,0 +1,12 @@
+
+
+class Song 
+  
+  attr_accessor :title, :artist 
+  
+  def initialize(title = "7/11")
+    @artist = artist
+    @title = title
+  end
+  
+end
