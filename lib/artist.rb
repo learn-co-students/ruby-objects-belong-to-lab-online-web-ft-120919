f23d5be7dc5bf29f571e047e3e22ce24cf30135a
Joanna E K <kalemajoanna@gmail.com>
2020-01-07 11:36:23 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist
+  attr_accessor :name 
+ 
+  # def initialize(name) 
+  #   @name = name
+  # end
+ 
+end
+ 
+# beyonce = Artist.new("Beyonce")
+# seven_eleven = Song.new("7/11")
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+require "pry"
+class Song
+ 
+  attr_accessor :title, :artist 
+ 
+ 
+
+end
+
+# song.artist.name
+# seven_eleven = Song.new("7/11")
+# seven_eleven.artist = "Beyonce"
+# seven_eleven.artist 
